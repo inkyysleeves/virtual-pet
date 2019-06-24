d3f4d7f5e0648dc550af185fdc639b21ae2ad252
nathan saide <saltsmokeandtime@gmail.com>
2019-06-24 14:55:03 +0100

    gave my pet an age

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -4,4 +4,23 @@ describe('constructor', () => {
 it('returns a new object', () => {
   expect(new Pet('Fido')).toBeInstanceOf(Object);
  });
+ it('sets the name property', () => {
+   const pet = new Pet('Fido');
+   expect(pet.name).toEqual('Fido');
+ });
+ describe('pets age', () => {
+it('gives the pet an age of 0', () =>{
+  const pet = new Pet('Fido');
+expect(pet.age).toEqual(0)
+ });
+describe('pets all growing', () => {
+ it('checks if the age has increased by 1', () => {
+   const pet = new Pet('Fido');
+
+   pet.growUp();
+
+   expect(pet.age).toEqual(1)
+         });
+      });
+   });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,9 @@
-const pet = require('./pet');
-function Pet() {}
-
+function Pet(name, age) {
+  this.name = name;
+  this.age = 0;
+  this.growUp = function() {
+    this.age +=1;
+  }
+};
 
 module.exports = Pet;
