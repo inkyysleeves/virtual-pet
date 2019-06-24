2a5e66556668a8cff86b55b313dae7e50fa460ac
nathan saide <saltsmokeandtime@gmail.com>
2019-06-24 20:44:24 +0100

    made some progress refactoring

--- __tests__/pet.tests.js
+++ __tests__/pet.tests.js
@@ -1,26 +1,71 @@
 const Pet = require('../src/pet');
 
-describe('constructor', () => {
+
 it('returns a new object', () => {
   expect(new Pet('Fido')).toBeInstanceOf(Object);
- });
- it('sets the name property', () => {
-   const pet = new Pet('Fido');
-   expect(pet.name).toEqual('Fido');
- });
- describe('pets age', () => {
-it('gives the pet an age of 0', () =>{
-  const pet = new Pet('Fido');
-expect(pet.age).toEqual(0)
- });
-describe('pets all growing', () => {
- it('checks if the age has increased by 1', () => {
-   const pet = new Pet('Fido');
-
-   pet.growUp();
-
-   expect(pet.age).toEqual(1)
-         });
-      });
-   });
 });
+it('sets the name property', () => {
+  const pet = new Pet('Fido');
+  expect(pet.name).toEqual('Fido');
+});
+
+
+it('gives the pet an age of 0', () => {
+  const pet = new Pet('Fido');
+  expect(pet.age).toEqual(0)
+});
+
+it('checks if the age has increased by 1', () => {
+  const pet = new Pet('Fido');
+
+  pet.growUp();
+
+  expect(pet.age).toEqual(1)
+});
+
+it('give the pet an initial hunger of 0', () => {
+  const pet = new Pet('Fido');
+  expect(pet.hunger).toEqual(0)
+});
+
+
+it('checks if the hunger has increased by 5', () => {
+  const pet = new Pet('Fido');
+
+  pet.growUp();
+
+  expect(pet.hunger).toEqual(5);
+});
+
+it('gives my pet a fitness level of 10', () => {
+  const pet = new Pet('Fido');
+  expect(pet.fitness).toEqual(4);
+});
+
+it('checks if the fitness has decreased by 3', () => {
+  const pet = new Pet('Fido');
+
+  pet.growUp();
+
+  expect(pet.fitness).toEqual(1);
+});
+describe('fitness gone up by 4', () => {
+it('checks the fitness has gone up by 4', () => {
+  const pet = new Pet('Fido');
+  const startingFitness = pet.fitness;
+
+  pet.walk();
+
+  expect(pet.fitness).toBe(startingFitness + 4);
+});
+});
+describe('fitness should never go above 10', () => {
+  it('checks fitness hasnt surpassed 10', () => {
+    const pet = new Pet('Fido');
+    for (let walk = 0; walk < 5; walk++) {
+      pet.walk();
+    }
+    expect(pet.fitness).toEqual(10);
+  });
+});
+
--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,22 @@
-function Pet(name, age) {
+function Pet(name, age, hunger, fitness) {
   this.name = name;
   this.age = 0;
-  this.growUp = function() {
-    this.age +=1;
+  this.hunger = 0;
+  this.fitness = 4;
+};
+Pet.prototype = {
+  growUp: function () {
+    this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
+  },
+  walk: function () {
+    this.fitness += 4;
+    if (this.fitness >= 10) {
+      return this.fitness = 10;
+    }
   }
 };
+
 
 module.exports = Pet;
